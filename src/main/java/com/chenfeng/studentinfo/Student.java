3a8c3583a38603a4126b50eccbce4fb44de22ce7
chenf24k <chenf24k@163.com>
2019-12-02 22:38:37 +0800

    计算学生的成绩

--- src/main/java/com/chenfeng/studentinfo/Student.java
+++ src/main/java/com/chenfeng/studentinfo/Student.java
@@ -1,13 +1,20 @@
 package com.chenfeng.studentinfo;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String name;
     private int credits;
+    private List<Grade> grades = new ArrayList<>();
+    private boolean isHonors = false;
     static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
 
     static final String IN_STATE = "CO";
     private String state = "";
+
+    enum Grade {A, B, C, D, F}
 
     public Student(final String name) {
         this.name = name;
@@ -43,8 +50,69 @@ public class Student {
         this.state = state;
     }
 
+    /**
+     * 判断学生是否是本州的学生
+     *
+     * @return boolean
+     */
     boolean isInState() {
         return state.toUpperCase().equals(Student.IN_STATE);
     }
 
+    /**
+     * 将成绩添加到grades
+     *
+     * @param grade
+     */
+    void addGrade(Grade grade) {
+        grades.add(grade);
+    }
+
+    /**
+     * 输出学生的平均成绩
+     *
+     * @return double
+     */
+    double getGpa() {
+        if (grades.isEmpty())
+            return 0.0;
+        double total = 0.0;
+        for (Grade grade : grades) {
+            total += gradePointsFor(grade);
+        }
+        return total / grades.size();
+    }
+
+    public void setHonors() {
+        isHonors = true;
+    }
+
+
+    /**
+     * 如何是荣誉学生，会比一般的多+1分
+     *
+     * @param grade 成绩
+     * @return int
+     */
+    int gradePointsFor(Grade grade) {
+        int points = basicGradePointsFor(grade);
+        if (isHonors)
+            if (points > 0)
+                points += 1;
+        return points;
+    }
+
+    /**
+     * 根据字母返回一个双精度的数值（成绩）
+     *
+     * @param grade 成绩
+     * @return int
+     */
+    private int basicGradePointsFor(Grade grade) {
+        if (grade == Grade.A) return 4;
+        if (grade == Grade.B) return 3;
+        if (grade == Grade.C) return 2;
+        if (grade == Grade.D) return 1;
+        return 0;
+    }
 }
--- src/test/java/com/chenfeng/studentinfo/StudentTest.java
+++ src/test/java/com/chenfeng/studentinfo/StudentTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
 
 @Test
 public class StudentTest {
+    private static final double GRADE_TOLERANCE = 0.05;
+
     public void testCreate() {
         final String firstStudentName = "Jane Doe";
         final Student firstStudent = new Student(firstStudentName);
@@ -52,12 +54,53 @@ public class StudentTest {
         Assert.assertTrue(student.isFullTime());
     }
 
-    public void testInState(){
+    public void testInState() {
         Student student = new Student("a");
         Assert.assertFalse(student.isInState());
         student.setState(Student.IN_STATE);
         Assert.assertTrue(student.isInState());
         student.setState("MD");
         Assert.assertFalse(student.isInState());
+    }
+
+    public void testCalculateGpa() {
+        // 在比较浮点数时，需要提供第是三个参数来表示允许的误差
+        Student student = new Student("a");
+        assertGpa(student, 0.0);
+        student.addGrade(Student.Grade.A);
+        assertGpa(student, 4.0);
+        student.addGrade(Student.Grade.B);
+        assertGpa(student, 3.5);
+        student.addGrade(Student.Grade.C);
+        assertGpa(student, 3.0);
+        student.addGrade(Student.Grade.D);
+        assertGpa(student, 2.5);
+        student.addGrade(Student.Grade.F);
+        assertGpa(student, 2.0);
+    }
+
+    private void assertGpa(Student student, double expectedGpa) {
+        Assert.assertEquals(student.getGpa(), expectedGpa, GRADE_TOLERANCE);
+    }
+
+    public void testCalculateHonorsStudentGpa() {
+        assertGpa(createHonorsStudent(), 0.0);
+        assertGpa(createHonorsStudent(Student.Grade.A), 5.0);
+        assertGpa(createHonorsStudent(Student.Grade.B), 4.0);
+        assertGpa(createHonorsStudent(Student.Grade.C), 3.0);
+        assertGpa(createHonorsStudent(Student.Grade.D), 2.0);
+        assertGpa(createHonorsStudent(Student.Grade.F), 0.0);
+    }
+
+    private Student createHonorsStudent(Student.Grade grade) {
+        Student student = createHonorsStudent();
+        student.addGrade(grade);
+        return student;
+    }
+
+    private Student createHonorsStudent() {
+        Student student = new Student("a");
+        student.setHonors();
+        return student;
     }
 }
